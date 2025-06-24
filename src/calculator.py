a79e2cb168e26e3d1364a0eb01b58bbb3c581a80
Михаил <TimCockk>
2025-06-24 19:16:57 +0300

    calculator module

---
+++ src/calculator.py
@@ -0,0 +1,25 @@
+def add(a: float, b: float) -> float:
+    """
+    Возвращает сумму двух чисел.
+
+    Args:
+        a (float): Первое число.
+        b (float): Второе число.
+
+    Returns:
+        float: Сумма чисел a и b.
+    """
+    return a + b
+
+def multiply(a: float, b: float) -> float:
+    """
+    Возвращает произведение двух чисел.
+
+    Args:
+        a (float): Первое число.
+        b (float): Второе число.
+
+    Returns:
+        float: Произведение чисел a и b.
+    """
+    return a * b
